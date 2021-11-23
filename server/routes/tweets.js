ca1736e4b6bb2416676cdf5d692c93c835ba3e04
NamSahng <namskygreen@naver.com>
2021-11-23 17:31:49 +0900

    my validator trial

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,7 +1,17 @@
 import express from "express";
 import "express-async-errors";
 import * as tweetController from "../controller/tweets.js";
+import { body, param, validationResult } from "express-validator";
+
 const router = express.Router();
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array() });
+};
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -11,12 +21,33 @@ router.get("/", tweetController.getTweets);
 router.get("/:id", tweetController.getTweet);
 
 // POST /tweeets
-router.post("/", tweetController.createTweet);
+router.post(
+  "/",
+  [
+    body("text").notEmpty().withMessage("Write sth plz!"),
+    body("username").notEmpty().withMessage("No username "),
+    body("name").notEmpty().withMessage("No name"),
+    validate,
+  ],
+  tweetController.createTweet
+);
 
 // PUT /tweets/:id
-router.put("/:id", tweetController.updateTweet);
+router.put(
+  "/:id",
+  [
+    param("id").trim().notEmpty(),
+    body("text").notEmpty().withMessage("Write sth plz!"),
+    validate,
+  ],
+  tweetController.updateTweet
+);
 
 // DELETE /tweets/:id
-router.delete("/:id", tweetController.deleteTweet);
+router.delete(
+  "/:id",
+  [param("id").trim().notEmpty(), validate],
+  tweetController.deleteTweet
+);
 
 export default router;
